6872da7970a19f25844642b3f106996ce9c8ffb8
meredith winchell <meredithwinchell@gmail.com>
2018-06-12 18:30:20 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,6 +1,32 @@
 class Dog
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
 
-  # Replace with Dog clas
+  def name= (name)
+    @name = name
+  end
 
+  def name
+    @name
+  end
+
+  def breed= (breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def age= (age)
+    @age = age
+  end
+
+  def age
+    @age
+  end 
 
 end
